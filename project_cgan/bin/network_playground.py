bc2429341f538f060e0921b04c30bed968664426
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-04-28 14:23:53 +0200

    loading a state dict should be possible now

--- project_cgan/bin/network_playground.py
+++ project_cgan/bin/network_playground.py
@@ -4,13 +4,22 @@ import logging
 import torchvision.utils as vision_utils
 from matplotlib import pyplot as plt
 import numpy as np
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
+    path = r"net_10.pt"
     batch_size = 64
+    feature_map_size = 64
+    color_channels = 3
     input_size = 100
-    net: GanGenerator = torch.load("net.pt")
+    net = GanGenerator(feature_map_size=feature_map_size,
+                       color_channels=color_channels,
+                       input_size=input_size)
+    net.load_state_dict(torch.load(path))
+    net.eval()
+    # net = torch.load(path)
     use_gpu = torch.cuda.is_available()
-    device: torch.device = torch.device("cuda:0" if use_gpu else "cpu")
+    device: torch.device = torch.device("cpu")
 
     noise = torch.randn(batch_size, 100, 1, 1, device=device)
     image = vision_utils.make_grid(net(noise).detach().cpu(), normalize=True)
