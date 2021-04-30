8105e910c8206421ca9f6b85ae4e1ec593365e07
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-04-30 13:18:59 +0200

    introduced CGan

--- project_cgan/lib/discriminator.py
+++ project_cgan/lib/discriminator.py
@@ -1,6 +1,20 @@
 import torch
 import torch.nn as nn
 from typing import *
+import argparse
+import os
+import numpy as np
+import math
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn.functional as F
+import torch
 
 
 def __gen_block(input_size, output_size, kernel, stride, padding, negative_slope: float,
@@ -71,36 +85,41 @@ class DCGanDiscriminator(nn.Module):
         return self.main(input_vector)
 
 
-class CganDiscriminator(DCGanDiscriminator):
-    r"""
-    roughly oriented on
-    https://github.com/Lornatang/CGAN-PyTorch/blob/master/cgan_pytorch/models.py
+class CGanGenerator(nn.Module):
+    """
+    loosely based on:
+    https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/cgan/cgan.py
     """
     def __init__(self,
-                 feature_map_size: int,
-                 input_channels: int,
-                 num_classes: int,
-                 kernel_size: Union[int, Tuple[int, int]] = 4,
-                 stride: Union[int, Tuple[int, int]] = 2,
-                 padding: Union[int, Tuple[int, int]] = 1,
-                 bias: bool = False,
-                 inplace: bool = True,
-                 negative_slope: float = 0.2):
-        super().__init__(
-                 feature_map_size=feature_map_size,
-                 input_channels=input_channels + num_classes,
-                 kernel_size=kernel_size,
-                 stride=stride,
-                 padding=padding,
-                 bias=bias,
-                 inplace=inplace,
-                 negative_slope=negative_slope)
-        self.label_embedding = nn.Embedding(num_classes, num_classes)
+                 classes: int,
+                 latent_dim: int = 100,
+                 img_shape: Optional[Tuple[int, int, int]] = None):
+        super(CGanGenerator, self).__init__()
+        if img_shape is None:
+            img_shape = (3, 64, 64)
 
-    def forward(self, input_vector, labels: list = None) -> torch.Tensor:
+        self.img_shape = img_shape
+        self.label_emb = nn.Embedding(classes, classes)
 
-        flattened_input = torch.flatten(input_vector, 1)
-        conditional = self.label_embedding(labels)
-        conditional_input = torch.cat([flattened_input, conditional], dim=-1)
-        output = self.main(conditional_input)
-        return output
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(latent_dim + classes, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, noise, labels):
+        # Concatenate label embedding and image to produce input
+        gen_input = torch.cat((self.label_emb(labels), noise), -1)
+        img = self.model(gen_input)
+        img = img.view(img.size(0), *self.img_shape)
+        return img
