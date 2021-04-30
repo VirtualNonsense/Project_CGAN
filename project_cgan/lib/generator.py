c5255679f53b28706449e5be2662a8b7f81b82a6
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-04-30 14:09:10 +0200

    introduced CGan generator

--- project_cgan/lib/generator.py
+++ project_cgan/lib/generator.py
@@ -1,5 +1,7 @@
 import torch.nn as nn
 from typing import *
+import torch
+import numpy as np
 
 
 class DCGanGenerator(nn.Module):
@@ -62,3 +64,44 @@ class DCGanGenerator(nn.Module):
 
     def forward(self, input_vector):
         return self.main(input_vector)
+
+
+class CGanGenerator(nn.Module):
+    """
+    loosely based on:
+    https://github.com/eriklindernoren/PyTorch-GAN/blob/master/implementations/cgan/cgan.py
+    """
+    def __init__(self,
+                 classes: int,
+                 latent_dim: int,
+                 img_shape: Optional[Tuple[int, int, int]] = None):
+        super(CGanGenerator, self).__init__()
+        if img_shape is None:
+            img_shape = (3, 64, 64)
+
+        self.img_shape = img_shape
+
+        self.label_emb = nn.Embedding(classes, classes)
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(latent_dim + classes, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
+            nn.Tanh()
+        )
+
+    def forward(self, noise, labels):
+        # Concatenate label embedding and image to produce input
+        gen_input = torch.cat((self.label_emb(labels), noise), -1)
+        img = self.model(gen_input)
+        img = img.view(img.size(0), *self.img_shape)
+        return img
