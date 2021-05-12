29e0caa8aef16ef249fee580faacadc7cf553da5
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2021-05-12 18:50:29 +0200

    rewrote generator for lightning

--- project_cgan/lib/generator.py
+++ project_cgan/lib/generator.py
@@ -1,64 +1,41 @@
+import os
+from argparse import ArgumentParser
+from collections import OrderedDict
+
+import numpy as np
+import torch
 import torch.nn as nn
-from typing import *
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+
+import pytorch_lightning as pl
 
 
-class GanGenerator(nn.Module):
-    def __init__(self,
-                 # number_of_gpus: int,
-                 feature_map_size: int,
-                 color_channels: int,
-                 input_size: int,
-                 kernel_size: Union[int, Tuple[int, int]] = 4,
-                 stride: Union[int, Tuple[int, int]] = 2,
-                 padding: Union[int, Tuple[int, int]] = 1,
-                 bias: bool = False):
-        super(GanGenerator, self).__init__()
-        # self.number_of_gpus = number_of_gpus
-        self.__input_size = input_size
-        self.__kernel_size = (kernel_size, kernel_size) if type(kernel_size) is int else kernel_size
-        self.__stride = (stride, stride) if type(stride) is int else stride
-        self.__padding = (padding, padding) if type(padding) is int else padding
-        self.main = nn.Sequential(
-            # input is Z, going into a convolution
-            # nn.ConvTranspose2d(input_size, feature_map_size * 16, kernel_size=self.__kernel_size, stride=self.__stride,
-            #                    padding=(0, 0),
-            #                    bias=bias),
-            # nn.BatchNorm2d(feature_map_size * 16),
-            # nn.ReLU(True),
-            nn.ConvTranspose2d(input_size, feature_map_size * 8, kernel_size=self.__kernel_size, stride=self.__stride,
-                               padding=(0, 0),
-                               bias=bias),
-            nn.BatchNorm2d(feature_map_size * 8),
-            nn.ReLU(True),
-            # state size. (ngf*8) x 4 x 4
-            nn.ConvTranspose2d(feature_map_size * 8, feature_map_size * 4, kernel_size=self.__kernel_size,
-                               stride=self.__stride,
-                               padding=self.__padding, bias=bias),
-            nn.BatchNorm2d(feature_map_size * 4),
-            nn.ReLU(True),
-            # state size. (ngf*4) x 8 x 8
-            nn.ConvTranspose2d(feature_map_size * 4, feature_map_size * 2, kernel_size=self.__kernel_size,
-                               stride=self.__stride,
-                               padding=self.__padding, bias=bias),
-            nn.BatchNorm2d(feature_map_size * 2),
-            nn.ReLU(True),
-            # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(feature_map_size * 2, feature_map_size, kernel_size=self.__kernel_size,
-                               stride=self.__stride,
-                               padding=self.__padding, bias=bias),
-            nn.BatchNorm2d(feature_map_size),
-            nn.ReLU(True),
-            # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d(feature_map_size, color_channels, kernel_size=self.__kernel_size, stride=self.__stride,
-                               padding=self.__padding,
-                               bias=bias),
+class Generator(nn.Module):
+    def __init__(self, latent_dim, img_shape):
+        super().__init__()
+        self.img_shape = img_shape
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(latent_dim, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, int(np.prod(img_shape))),
             nn.Tanh()
-            # state size. (nc) x 64 x 64
         )
 
-    @property
-    def input_size(self):
-        return self.__input_size
-
-    def forward(self, input_vector):
-        return self.main(input_vector)
+    def forward(self, z):
+        img = self.model(z)
+        img = img.view(img.size(0), *self.img_shape)
+        return img
