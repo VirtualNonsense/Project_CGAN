d66e98d5fa4b97f1c57a05af746de79534bf0ffd
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2021-05-12 18:50:21 +0200

    rewrote discriminator for lightning

--- project_cgan/lib/discriminator.py
+++ project_cgan/lib/discriminator.py
@@ -1,106 +1,33 @@
+import os
+from argparse import ArgumentParser
+from collections import OrderedDict
+
+import numpy as np
 import torch
 import torch.nn as nn
-from typing import *
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import MNIST
+
+import pytorch_lightning as pl
 
 
-def __gen_block(input_size, output_size, kernel, stride, padding, negative_slope: float,
-                inplace: bool, bias: bool, batch_norm: bool = True):
-    if batch_norm:
-        return [
-            nn.Conv2d(input_size, output_size, kernel_size=kernel, stride=stride, padding=padding, bias=bias),
-            nn.BatchNorm2d(output_size),
-            nn.LeakyReLU(negative_slope, inplace=inplace)
-        ]
+class Discriminator(nn.Module):
+    def __init__(self, img_shape):
+        super().__init__()
 
-    return [
-        nn.Conv2d(input_size, output_size, kernel_size=kernel, stride=stride, padding=padding, bias=bias),
-        nn.LeakyReLU(negative_slope, inplace=inplace)
-    ]
-
-
-def _gen_layers(layer_count: int, input_layer_size, output_layer_size, map_size, kernel, padding, stride,
-                inplace: bool, bias: bool, negative_slope: float) -> List[any]:
-    layers = __gen_block(input_layer_size, map_size, kernel=kernel, stride=stride, padding=padding,
-                         inplace=inplace, negative_slope=negative_slope, bias=bias,
-                         batch_norm=False)
-    new_map_size = map_size * 2
-    if layer_count > 2:
-        for i in range(layer_count - 2):
-            layers += __gen_block(map_size, new_map_size, kernel=kernel, stride=stride,
-                                  padding=padding,
-                                  inplace=inplace, negative_slope=negative_slope, bias=bias)
-            map_size = new_map_size
-            new_map_size *= 2
-    layers += [
-        nn.Conv2d(map_size, output_layer_size, kernel_size=kernel, stride=(1, 1), padding=(0, 0), bias=bias),
-        nn.Sigmoid()
-    ]
-    return layers
-
-
-class GanDiscriminator(nn.Module):
-    def __init__(self,
-                 # number_of_gpus: int,
-                 feature_map_size: int,
-                 input_channels: int,
-                 kernel_size: Union[int, Tuple[int, int]] = 4,
-                 stride: Union[int, Tuple[int, int]] = 2,
-                 padding: Union[int, Tuple[int, int]] = 1,
-                 bias: bool = False,
-                 inplace: bool = True,
-                 negative_slope: float = 0.2):
-        super(GanDiscriminator, self).__init__()
-        self.__kernel_size = (kernel_size, kernel_size) if type(kernel_size) is int else kernel_size
-        self.__stride = (stride, stride) if type(stride) is int else stride
-        self.__padding = (padding, padding) if type(padding) is int else padding
-        # self.number_of_gpus = number_of_gpus
-        self.main = nn.Sequential(
-            *_gen_layers(5,
-                         input_channels,
-                         1,
-                         feature_map_size,
-                         self.__kernel_size,
-                         self.__padding,
-                         self.__stride,
-                         bias=bias,
-                         inplace=inplace,
-                         negative_slope=negative_slope)
+        self.model = nn.Sequential(
+            nn.Linear(int(np.prod(img_shape)), 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
         )
 
-    def forward(self, input_vector) -> torch.Tensor:
-        return self.main(input_vector)
-
-
-class CganDiscriminator(GanDiscriminator):
-    r"""
-    roughly oriented on
-    https://github.com/Lornatang/CGAN-PyTorch/blob/master/cgan_pytorch/models.py
-    """
-    def __init__(self,
-                 feature_map_size: int,
-                 input_channels: int,
-                 num_classes: int,
-                 kernel_size: Union[int, Tuple[int, int]] = 4,
-                 stride: Union[int, Tuple[int, int]] = 2,
-                 padding: Union[int, Tuple[int, int]] = 1,
-                 bias: bool = False,
-                 inplace: bool = True,
-                 negative_slope: float = 0.2):
-        super().__init__(
-                 feature_map_size=feature_map_size,
-                 input_channels=input_channels + num_classes,
-                 kernel_size=kernel_size,
-                 stride=stride,
-                 padding=padding,
-                 bias=bias,
-                 inplace=inplace,
-                 negative_slope=negative_slope)
-        self.label_embedding = nn.Embedding(num_classes, num_classes)
-
-    def forward(self, input_vector, labels: list = None) -> torch.Tensor:
-
-        flattened_input = torch.flatten(input_vector, 1)
-        conditional = self.label_embedding(labels)
-        conditional_input = torch.cat([flattened_input, conditional], dim=-1)
-        output = self.main(conditional_input)
-        return output
+    def forward(self, img):
+        img_flat = img.view(img.size(0), -1)
+        validity = self.model(img_flat)
+        return validity
