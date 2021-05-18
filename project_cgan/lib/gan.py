9136e51ab9aaa5783ed323ea7db1064f95f38d77
pillhoferjo74678 <pillhoferjo74678@th-nuernberg.de>
2021-05-18 15:49:23 +0200

    removed tensorboard logging until we get the tune to work

--- project_cgan/lib/gan.py
+++ project_cgan/lib/gan.py
@@ -72,7 +72,7 @@ class GAN(pl.LightningModule):
                 'progress_bar': tqdm_dict,
                 'log': tqdm_dict
             })
-            writer.add_scalar('Generator Loss', g_loss, self.current_epoch)
+            # writer.add_scalar('Generator Loss', g_loss, self.current_epoch)
             return output
 
         # train discriminator
@@ -99,7 +99,7 @@ class GAN(pl.LightningModule):
                 'progress': tqdm_dict,
                 'log': tqdm_dict
             })
-            writer.add_scalar('Discriminator Loss', d_loss, self.current_epoch)
+            # writer.add_scalar('Discriminator Loss', d_loss, self.current_epoch)
             return output
 
     def configure_optimizers(self):
@@ -118,9 +118,9 @@ class GAN(pl.LightningModule):
         sample_imgs = self(z)
         grid = torchvision.utils.make_grid(sample_imgs)
         # logger.experiment.add_image('generated_images', grid, self.current_epoch)
-        writer.add_image('images', grid, global_step=self.current_epoch)
-        writer.add_graph(self.discriminator, input_to_model=sample_imgs)
-        writer.add_scalar('Lr', self.hparams.lr)
+        # writer.add_image('images', grid, global_step=self.current_epoch)
+        # writer.add_graph(self.discriminator, input_to_model=sample_imgs)
+        # writer.add_scalar('Lr', self.hparams.lr)
         # writer.add_hparams({
         #     'lr': self.hparams.lr,
         #     'bsize': self.batch_size,
