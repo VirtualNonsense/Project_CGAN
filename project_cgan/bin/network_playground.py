cbd4d869d78dbd9fa14afe8a8c89061bcb99f401
Andreas Nachtmann <virtualnonsense@mailbox.org>
2021-06-19 15:13:09 +0200

    example config for pretrained cdcgan

--- project_cgan/bin/network_playground.py
+++ project_cgan/bin/network_playground.py
@@ -1,28 +1,41 @@
 import torch
-from project_cgan.lib.generator import DCGanGenerator
+# from project_cgan.lib.generator import DCGanGenerator
+from project_cgan.lib.c_dcgan import Generator
 import logging
 import torchvision.utils as vision_utils
 from matplotlib import pyplot as plt
 import numpy as np
+import torchvision
 
 if __name__ == '__main__':
+
     logging.basicConfig(level=logging.DEBUG)
-    path = r"g_net_500.pt"
-    batch_size = 64
+    path = r"gen_15901_256_100.pkl"
+    batch_size = 1
     feature_map_size = 64
     color_channels = 3
     input_size = 100
-    net = DCGanGenerator(feature_map_size=feature_map_size,
-                         color_channels=color_channels,
-                         input_size=input_size)
-    net.load_state_dict(torch.load(path))
-    net.eval()
+    rows = 10
+    num_filters = [1024, 512, 256, 128, 64, 32]
+    amount_classes = 5
     # net = torch.load(path)
     use_gpu = torch.cuda.is_available()
     device: torch.device = torch.device("cpu")
+    net = Generator(100, amount_classes, num_filters, 3, used_device=device)
+    net.load_state_dict(torch.load(path))
+    net.eval()
+    # generator representation
+    # each class has it's own 1 layer within this tensor.
+    fixed = torch.tensor(
+        [i % amount_classes for i in range(rows * amount_classes)],
+        device=device, dtype=torch.long)
 
-    noise = torch.randn(batch_size, 100, 1, 1, device=device)
-    image = vision_utils.make_grid(net(noise).detach().cpu(), normalize=True)
+    noise = torch.randn(rows * amount_classes, 100, 1, 1, device=device)
+
+    img = (net(noise, fixed).detach().cpu() + 1) / 2
+
+    grid = torchvision.utils.make_grid(img, nrow=amount_classes)
+    image = vision_utils.make_grid(grid, normalize=True)
     plt.figure()
     plt.imshow(np.transpose(image, (1, 2, 0)))
     plt.show()
